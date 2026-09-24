2188642f0267a5895743f91e0cb58e04edd2672f
Sujai <sujaiit0696@gmail.com>
2026-09-24 15:35:25 +0530

    fix(setup): export after_migrate so bench migrate hook resolves

--- bizaxl_hrms/setup/__init__.py
+++ bizaxl_hrms/setup/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-from .install import after_install, before_install
+from .install import after_install, after_migrate, before_install
 
-__all__ = ["before_install", "after_install"]
+__all__ = ["before_install", "after_install", "after_migrate"]
